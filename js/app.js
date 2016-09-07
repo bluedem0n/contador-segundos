5149e7cf4d75972fbc692bf6e6e88f3c1aae4603
Sthefany Floriano <sthefanyflorianog@gmail.com>
2016-09-07 09:51:32 -0500

    Subiendo repo

---
+++ js/app.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function() {
+    var button = document.getElementById("calcular");
+    button.addEventListener("click", function() {
+        var segundos = parseInt(document.getElementById("segundos").value);
+        var numero= parseInt(document.getElementById("numero").value);
+        contador(segundos,numero);
+    });
+    function contador(segundos, numero) {
+        setInterval(function(){document.write(numero++ + "<br>")}, segundos*1000);
+    }
+});
